8979f5fffe755bd526fb5d2324a36f8a6585c3a5
Gustave Monce <gustave.monce@outlook.com>
2023-11-05 20:45:05 +0100

    Initial code

---
+++ QCBSPRenamer/Program.cs
@@ -0,0 +1,281 @@
+﻿using SharpCompress.Archives;
+using SharpCompress.Archives.Zip;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace QCBSPRenamer
+{
+    internal class Program
+    {
+        private static string GetContentString(IArchive zipArchive, char archiveSep)
+        {
+            IArchiveEntry contentEntry = zipArchive.Entries.First(entry => !entry.IsDirectory && entry.Key.EndsWith("contents.xml") && entry.Key.Count(x => x == archiveSep) <= 1);
+
+            using Stream contentsXmlFileStream = new MemoryStream();
+            contentEntry.WriteTo(contentsXmlFileStream);
+            contentsXmlFileStream.Seek(0, SeekOrigin.Begin);
+
+            using BinaryReader br = new BinaryReader(contentsXmlFileStream);
+            byte[] fileContent = br.ReadBytes((int)contentsXmlFileStream.Length);
+            string strContent = Encoding.UTF8.GetString(fileContent);
+
+            XmlSerializer ser = new(typeof(Contents));
+            using XmlReader reader = XmlReader.Create(new StringReader(strContent));
+
+            if (ser.Deserialize(reader) is Contents contents)
+            {
+                foreach (Build buildContent in contents.Builds_flat.Build)
+                {
+                    Console.WriteLine(buildContent.Name + "\t: " + buildContent.Build_id);
+                }
+                Console.WriteLine();
+
+                Build commonBuild = contents.Builds_flat.Build.First(x => x.Name.Equals("common"));
+                if (commonBuild.Build_id != ".")
+                {
+                    return commonBuild.Build_id;
+                }
+            }
+
+            throw new Exception("Unexpected Contents.xml format!");
+        }
+
+        private static List<Dictionary<string, string>> GetContentsFromAbout(string[] lines)
+        {
+            bool IsCurrentlyInTrElement = false;
+            int trStart = 0;
+
+            List<string> currentHeaders = new();
+            List<Dictionary<string, string>> elements = new();
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string currentLine = lines[i].Trim();
+
+                if (!IsCurrentlyInTrElement)
+                {
+                    if (currentLine.Contains("<tr>"))
+                    {
+                        IsCurrentlyInTrElement = true;
+                        trStart = i + 1;
+                    }
+                }
+                else
+                {
+                    if (currentLine.Contains("</tr>"))
+                    {
+                        IsCurrentlyInTrElement = false;
+                        List<string> elementsIntr = lines[trStart..(i - 1)].Select(x => x.Trim()).Where(x => !string.IsNullOrEmpty(x)).ToList();
+                        trStart = 0; // Reset for good measure
+
+                        // <th> -> header
+                        // <td> -> element
+
+                        bool header = elementsIntr.Any(x => x.Contains("<th"));
+                        if (header)
+                        {
+                            currentHeaders.Clear();
+                        }
+
+                        Dictionary<string, string> contentElement = new Dictionary<string, string>();
+
+                        for (int i1 = 0; i1 < elementsIntr.Count; i1++)
+                        {
+                            string element = elementsIntr[i1];
+                            string normalizedElement = element.Split(">")[1].Split("<")[0].Trim();
+
+                            if (header)
+                            {
+                                currentHeaders.Add(normalizedElement);
+                            }
+                            else
+                            {
+                                contentElement.Add(currentHeaders[i1], normalizedElement);
+                            }
+                        }
+
+                        if (!header)
+                        {
+                            elements.Add(contentElement);
+                        }
+                    }
+                }
+            }
+
+            return elements;
+        }
+
+        private static readonly string DistroPath = "Distro Path";
+        private static readonly string BuildLabel = "Build/Label";
+
+        private static string GetAboutString(IArchive zipArchive, char archiveSep)
+        {
+            IArchiveEntry contentEntry = zipArchive.Entries.First(entry => !entry.IsDirectory && entry.Key.EndsWith("about.html") && entry.Key.Count(x => x == archiveSep) <= 1);
+
+            using Stream aboutHtmlFileStream = new MemoryStream();
+            contentEntry.WriteTo(aboutHtmlFileStream);
+            aboutHtmlFileStream.Seek(0, SeekOrigin.Begin);
+
+            using BinaryReader reader = new BinaryReader(aboutHtmlFileStream);
+            byte[] fileContent = reader.ReadBytes((int)aboutHtmlFileStream.Length);
+            string strContent = Encoding.UTF8.GetString(fileContent);
+
+            string[] lines = strContent.Replace("\r", "").Split("\n").ToArray();
+            List<Dictionary<string, string>> content = GetContentsFromAbout(lines);
+
+            foreach (Dictionary<string, string> buildContent in content)
+            {
+                if (buildContent.ContainsKey(DistroPath))
+                {
+                    Console.WriteLine(buildContent[DistroPath] + "\t: " + buildContent[BuildLabel]);
+                }
+            }
+            Console.WriteLine();
+
+            if (content.Any(x => x.ContainsKey(DistroPath) && x[DistroPath] == "common" && x[BuildLabel] != "."))
+            {
+                Dictionary<string, string> commonBuild = content.First(x => x.ContainsKey(DistroPath) && x[DistroPath] == "common");
+                return commonBuild[BuildLabel];
+            }
+
+            if (content.Any(x => x.ContainsKey(BuildLabel) && x[BuildLabel].Contains("STD")))
+            {
+                Dictionary<string, string> commonBuild = content.First(x => x.ContainsKey(BuildLabel) && x[BuildLabel].Contains("STD"));
+                return commonBuild[BuildLabel];
+            }
+
+            throw new Exception("Unexpected about.html format!");
+        }
+
+        private static string GetVersionString(IArchive zipArchive, char archiveSep)
+        {
+            IArchiveEntry[] files = zipArchive.Entries.Where(entry => !entry.IsDirectory &&
+                entry.Key.Contains("WP" + archiveSep + "prebuilt" + archiveSep, StringComparison.InvariantCultureIgnoreCase) &&
+                entry.Key.EndsWith(".inf", StringComparison.InvariantCultureIgnoreCase)).ToArray();
+
+            HashSet<string> versionStrings = new();
+
+            foreach (IArchiveEntry file in files)
+            {
+                using Stream infFileStream = new MemoryStream();
+                file.WriteTo(infFileStream);
+                infFileStream.Seek(0, SeekOrigin.Begin);
+
+                using BinaryReader reader = new BinaryReader(infFileStream);
+                byte[] fileContent = reader.ReadBytes((int)infFileStream.Length);
+                string strContent = Encoding.UTF8.GetString(fileContent);
+
+                string[] lines = strContent.Replace("\r", "").Split("\n").ToArray();
+
+                string DriverVer = lines
+                    .First(x => x
+                    .Contains("DriverVer", StringComparison.InvariantCultureIgnoreCase))
+                    .Split("=")[^1].Replace("\0", "")
+                    .Trim();
+                string buildNumber = DriverVer.Split(',')[1];
+
+                string[] dateElements = DriverVer.Split(',')[0].Trim().Split('/');
+
+                string date = $"{dateElements[2]}.{dateElements[0]}.{dateElements[1]}";
+                string normalizedVersion = date + "-" + buildNumber;
+
+                if (buildNumber.StartsWith("1.0."))
+                {
+                    versionStrings.Add(normalizedVersion);
+                }
+            }
+
+            if (versionStrings.Count > 0)
+            {
+                string[] finalArrayOfVersions = versionStrings.Order().ToArray();
+                return finalArrayOfVersions[^1];
+            }
+
+            throw new Exception("Unexpected INF format!");
+        }
+
+        private static string GetWPBSPVersionString(IArchive zipArchive, char archiveSep)
+        {
+            string buildId = "";
+            string versionId = "";
+
+            try
+            {
+                buildId = GetContentString(zipArchive, archiveSep);
+            }
+            catch
+            {
+            }
+
+            if (string.IsNullOrEmpty(buildId))
+            {
+                try
+                {
+                    buildId = GetAboutString(zipArchive, archiveSep);
+                }
+                catch
+                {
+                }
+            }
+
+            if (string.IsNullOrEmpty(buildId))
+            {
+                throw new Exception("Unknown build id!");
+            }
+
+            try
+            {
+                versionId = GetVersionString(zipArchive, archiveSep);
+            }
+            catch
+            {
+            }
+
+            if (string.IsNullOrEmpty(versionId))
+            {
+                return buildId;
+            }
+
+            return $"{versionId}.{buildId}";
+        }
+
+        private static string GetNewFileName(string archive)
+        {
+            using IArchive zipArchive = ZipArchive.Open(archive);
+
+            string archiveName = Path.GetFileName(archive);
+            string version = GetWPBSPVersionString(zipArchive, '/');
+
+            string normalizedHashName = archiveName.Replace(version + "-", "");
+            string newArchiveName = version + "-" + normalizedHashName;
+            Console.WriteLine("Old: " + archiveName);
+            Console.WriteLine("New: " + newArchiveName);
+
+            return newArchiveName;
+        }
+
+        static void Main(string[] args)
+        {
+            string inputDirectory = args[0];
+            string[] archives = Directory.EnumerateFiles(inputDirectory, "*.zip").ToArray();
+
+            foreach (string archive in archives)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Processing: " + archive);
+                Console.WriteLine();
+
+                try
+                {
+                    string newArchiveName = GetNewFileName(archive);
+                    File.Move(archive, Path.Combine(inputDirectory, newArchiveName));
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}
---
+++ QCBSPRenamer/XmlClasses.cs
@@ -0,0 +1,655 @@
+﻿using System.Xml.Serialization;
+
+namespace QCBSPRenamer
+{
+
+    [XmlRoot(ElementName = "component")]
+    public class Component
+    {
+        [XmlElement(ElementName = "name")]
+        public string Name
+        {
+            get; set;
+        }
+        [XmlElement(ElementName = "flavor")]
+        public string Flavor
+        {
+            get; set;
+        }
+    }
+
+    [XmlRoot(ElementName = "pf")]
+    public class Pf
+    {
+        [XmlElement(ElementName = "name")]
+        public string Name
+        {
+            get; set;
+        }
+        [XmlElement(ElementName = "component")]
+        public List<Component> Component
+        {
+            get; set;
+        }
+    }
+
+    [XmlRoot(ElementName = "product_flavors")]
+    public class Product_flavors
+    {
+        [XmlElement(ElementName = "pf")]
+        public List<Pf> Pf
+        {
+            get; set;
+        }
+        [XmlAttribute(AttributeName = "cmm_pf_var")]
+        public string Cmm_pf_var
+        {
+            get; set;
+        }
+    }
+
+    [XmlRoot(ElementName = "hlos_type")]
+    public class Hlos_type
+    {
+        [XmlAttribute(AttributeName = "cmm_var")]
+        public string Cmm_var
+        {
+            get; set;
+        }
+        [XmlText]
+        public string Text
+        {
+            get; set;
+        }
+    }
+
+    [XmlRoot(ElementName = "chipid")]
+    public class Chipid
+    {
+        [XmlAttribute(AttributeName = "cmm_var")]
+        public string Cmm_var
+        {
+            get; set;
+        }
+        [XmlText]
+        public string Text
+        {
+            get; set;
+        }
+    }
+
+    [XmlRoot(ElementName = "product_info")]
+    public class Product_info
+    {
+        [XmlElement(ElementName = "product_name")]
+        public string Product_name
+        {
+            get; set;
+        }
+        [XmlElement(ElementName = "hlos_type")]
+        public Hlos_type Hlos_type
+        {
+            get; set;
+        }
+        [XmlElement(ElementName = "chipid")]
+        public Chipid Chipid
+        {
+            get; set;
+        }
+        [XmlElement(ElementName = "additional_chipid")]
+        public string Additional_chipid
+        {
+            get; set;
+        }
+    }
+
+    [XmlRoot(ElementName = "partition")]
+    public class Partition
+    {
+        [XmlAttribute(AttributeName = "fastboot_erase")]
+        public string Fastboot_erase
+        {
+            get; set;
+        }
+        [XmlText]
+        public string Text
+        {
+            get; set;
+        }
+    }
+
+    [XmlRoot(ElementName = "partition_info")]
+    public class Partition_info
+    {
+        [XmlElement(ElementName = "partition")]
+        public List<Partition> Partition
+        {
+            get; set;
+        }
+    }
+
+    [XmlRoot(ElementName = "download_file")]
+    public class Download_file
+    {
+        [XmlElement(ElementName = "file_name")]
+        public string File_name
+        {
+            get; set;
+        }
+        [XmlAttribute(AttributeName = "ignore")]
+        public string Ignore
+        {
+            get; set;
+        }
+        [XmlAttribute(AttributeName = "storage_type")]
+        public string Storage_type
+        {
+            get; set;
+        }
+        [XmlAttribute(AttributeName = "minimized")]
+        public string Minimized
+        {
+            get; set;
+        }
+        [XmlAttribute(AttributeName = "fastboot")]
+        public string Fastboot
+        {
+            get; set;
+        }
+        [XmlElement(ElementName = "file_path")]
+        public File_path File_path
+        {
+            get; set;
+        }
+        [XmlAttribute(AttributeName = "cmm_file_var")]
+        public string Cmm_file_var
+        {
+            get; set;
+        }
+        [XmlAttribute(AttributeName = "fastboot_complete")]
+        public string Fastboot_complete
+        {
+            get; set;
+        }
+        [XmlAttribute(AttributeName = "gpt_file")]
+        public string Gpt_file
+        {
+            get; set;
+        }
+        [XmlAttribute(AttributeName = "form_factor")]
+        public string Form_factor
+        {
+            get; set;
+        }
+        [XmlAttribute(AttributeName = "fat_file")]
+        public string Fat_file
+        {
+            get; set;
+        }
+        [XmlAttribute(AttributeName = "pil_split")]
+        public string Pil_split
+        {
+            get; set;
+        }
+        [XmlAttribute(AttributeName = "clean_for_release")]
+        public string Clean_for_release
+        {
+            get; set;
+        }
+        [XmlAttribute(AttributeName = "symbol")]
+        public string Symbol
+        {
+            get; set;
+        }
+        [XmlAttribute(AttributeName = "fastboot_rumi")]
+        public string Fastboot_rumi
+        {
+            get; set;
+        }
+    }
+
+    [XmlRoot(ElementName = "file_path")]
+    public class File_path
+    {
+        [XmlAttribute(AttributeName = "flavor")]
+        public string Flavor
+        {
+            get; set;
+        }
+        [XmlText]
+        public string Text
+        {
+            get; set;
+        }
+    }
+
+    [XmlRoot(ElementName = "partition_file")]
+    public class Partition_file
+    {
+        [XmlElement(ElementName = "file_name")]
+        public string File_name
+        {
+            get; set;
+        }
+        [XmlElement(ElementName = "file_path")]
+        public string File_path
+        {
+            get; set;
+        }
+        [XmlAttribute(AttributeName = "storage_type")]
+        public string Storage_type
+        {
+            get; set;
+        }
+    }
+
+    [XmlRoot(ElementName = "partition_patch_file")]
+    public class Partition_patch_file
+    {
+        [XmlElement(ElementName = "file_name")]
+        public string File_name
+        {
+            get; set;
+        }
+        [XmlElement(ElementName = "file_path")]
+        public string File_path
+        {
+            get; set;
+        }
+        [XmlAttribute(AttributeName = "storage_type")]
+        public string Storage_type
+        {
+            get; set;
+        }
+    }
+
+    [XmlRoot(ElementName = "build")]
+    public class Build
+    {
+        [XmlElement(ElementName = "name")]
+        public string Name
+        {
+            get; set;
+        }
+        [XmlElement(ElementName = "role")]
+        public string Role
+        {
+            get; set;
+        }
+        [XmlElement(ElementName = "chipset")]
+        public string Chipset
+        {
+            get; set;
+        }
+        [XmlElement(ElementName = "build_id")]
+        public string Build_id
+        {
+            get; set;
+        }
+        [XmlElement(ElementName = "image_dir")]
+        public string Image_dir
+        {
+            get; set;
+        }
+        [XmlElement(ElementName = "release_path")]
+        public string Release_path
+        {
+            get; set;
+        }
+        [XmlElement(ElementName = "download_file")]
+        public List<Download_file> Download_file
+        {
+            get; set;
+        }
+        [XmlElement(ElementName = "partition_file")]
+        public List<Partition_file> Partition_file
+        {
+            get; set;
+        }
+        [XmlElement(ElementName = "partition_patch_file")]
+        public List<Partition_patch_file> Partition_patch_file
+        {
+            get; set;
+        }
+        [XmlElement(ElementName = "buildfile_path")]
+        public string Buildfile_path
+        {
+            get; set;
+        }
+        [XmlElement(ElementName = "build_command")]
+        public string Build_command
+        {
+            get; set;
+        }
+        [XmlElement(ElementName = "windows_root_path")]
+        public Windows_root_path Windows_root_path
+        {
+            get; set;
+        }
+        [XmlElement(ElementName = "linux_root_path")]
+        public Linux_root_path Linux_root_path
+        {
+            get; set;
+        }
+        [XmlElement(ElementName = "file_ref")]
+        public List<File_ref> File_ref
+        {
+            get; set;
+        }
+        [XmlElement(ElementName = "short_build_path")]
+        public Short_build_path Short_build_path
+        {
+            get; set;
+        }
+        [XmlElement(ElementName = "device_programmer")]
+        public Device_programmer Device_programmer
+        {
+            get; set;
+        }
+        [XmlElement(ElementName = "flash_programmer")]
+        public Flash_programmer Flash_programmer
+        {
+            get; set;
+        }
+    }
+
+    [XmlRoot(ElementName = "windows_root_path")]
+    public class Windows_root_path
+    {
+        [XmlAttribute(AttributeName = "cmm_root_path_var")]
+        public string Cmm_root_path_var
+        {
+            get; set;
+        }
+        [XmlText]
+        public string Text
+        {
+            get; set;
+        }
+    }
+
+    [XmlRoot(ElementName = "linux_root_path")]
+    public class Linux_root_path
+    {
+        [XmlAttribute(AttributeName = "cmm_root_path_var")]
+        public string Cmm_root_path_var
+        {
+            get; set;
+        }
+        [XmlText]
+        public string Text
+        {
+            get; set;
+        }
+    }
+
+    [XmlRoot(ElementName = "file_ref")]
+    public class File_ref
+    {
+        [XmlElement(ElementName = "file_name")]
+        public string File_name
+        {
+            get; set;
+        }
+        [XmlAttribute(AttributeName = "ignore")]
+        public string Ignore
+        {
+            get; set;
+        }
+        [XmlAttribute(AttributeName = "minimized")]
+        public string Minimized
+        {
+            get; set;
+        }
+        [XmlAttribute(AttributeName = "fat_file")]
+        public string Fat_file
+        {
+            get; set;
+        }
+        [XmlAttribute(AttributeName = "sub_dir")]
+        public string Sub_dir
+        {
+            get; set;
+        }
+        [XmlAttribute(AttributeName = "debug_script")]
+        public string Debug_script
+        {
+            get; set;
+        }
+        [XmlElement(ElementName = "file_path")]
+        public File_path File_path
+        {
+            get; set;
+        }
+        [XmlAttribute(AttributeName = "clean_for_release")]
+        public string Clean_for_release
+        {
+            get; set;
+        }
+        [XmlAttribute(AttributeName = "cmm_file_var")]
+        public string Cmm_file_var
+        {
+            get; set;
+        }
+        [XmlAttribute(AttributeName = "storage_type")]
+        public string Storage_type
+        {
+            get; set;
+        }
+        [XmlAttribute(AttributeName = "pil_split")]
+        public string Pil_split
+        {
+            get; set;
+        }
+        [XmlAttribute(AttributeName = "adspso_signed")]
+        public string Adspso_signed
+        {
+            get; set;
+        }
+        [XmlAttribute(AttributeName = "fat_file_btfm")]
+        public string Fat_file_btfm
+        {
+            get; set;
+        }
+        [XmlAttribute(AttributeName = "cdspso_signed")]
+        public string Cdspso_signed
+        {
+            get; set;
+        }
+    }
+
+    [XmlRoot(ElementName = "short_build_path")]
+    public class Short_build_path
+    {
+        [XmlAttribute(AttributeName = "cmm_var")]
+        public string Cmm_var
+        {
+            get; set;
+        }
+        [XmlText]
+        public string Text
+        {
+            get; set;
+        }
+    }
+
+    [XmlRoot(ElementName = "device_programmer")]
+    public class Device_programmer
+    {
+        [XmlElement(ElementName = "file_name")]
+        public string File_name
+        {
+            get; set;
+        }
+        [XmlElement(ElementName = "file_path")]
+        public string File_path
+        {
+            get; set;
+        }
+        [XmlAttribute(AttributeName = "minimized")]
+        public string Minimized
+        {
+            get; set;
+        }
+    }
+
+    [XmlRoot(ElementName = "flash_programmer")]
+    public class Flash_programmer
+    {
+        [XmlElement(ElementName = "file_name")]
+        public string File_name
+        {
+            get; set;
+        }
+        [XmlElement(ElementName = "file_path")]
+        public string File_path
+        {
+            get; set;
+        }
+        [XmlAttribute(AttributeName = "minimized")]
+        public string Minimized
+        {
+            get; set;
+        }
+    }
+
+    [XmlRoot(ElementName = "builds_flat")]
+    public class Builds_flat
+    {
+        [XmlElement(ElementName = "build")]
+        public List<Build> Build
+        {
+            get; set;
+        }
+    }
+
+    [XmlRoot(ElementName = "version")]
+    public class Version
+    {
+        [XmlAttribute(AttributeName = "cmm_var")]
+        public string Cmm_var
+        {
+            get; set;
+        }
+        [XmlText]
+        public string Text
+        {
+            get; set;
+        }
+    }
+
+    [XmlRoot(ElementName = "tool")]
+    public class Tool
+    {
+        [XmlElement(ElementName = "name")]
+        public string Name
+        {
+            get; set;
+        }
+        [XmlElement(ElementName = "version")]
+        public Version Version
+        {
+            get; set;
+        }
+        [XmlElement(ElementName = "path")]
+        public string Path
+        {
+            get; set;
+        }
+        [XmlElement(ElementName = "build")]
+        public string Build
+        {
+            get; set;
+        }
+    }
+
+    [XmlRoot(ElementName = "external_tools")]
+    public class External_tools
+    {
+        [XmlElement(ElementName = "tool")]
+        public List<Tool> Tool
+        {
+            get; set;
+        }
+    }
+
+    [XmlRoot(ElementName = "step")]
+    public class Step
+    {
+        [XmlElement(ElementName = "params")]
+        public string Params
+        {
+            get; set;
+        }
+        [XmlElement(ElementName = "exec_dir")]
+        public string Exec_dir
+        {
+            get; set;
+        }
+        [XmlElement(ElementName = "tool_name")]
+        public string Tool_name
+        {
+            get; set;
+        }
+        [XmlAttribute(AttributeName = "type")]
+        public string Type
+        {
+            get; set;
+        }
+    }
+
+    [XmlRoot(ElementName = "workflow")]
+    public class Workflow
+    {
+        [XmlElement(ElementName = "tool")]
+        public List<Tool> Tool
+        {
+            get; set;
+        }
+        [XmlElement(ElementName = "step")]
+        public List<Step> Step
+        {
+            get; set;
+        }
+    }
+
+    [XmlRoot(ElementName = "contents")]
+    public class Contents
+    {
+        [XmlElement(ElementName = "product_flavors")]
+        public Product_flavors Product_flavors
+        {
+            get; set;
+        }
+        [XmlElement(ElementName = "product_info")]
+        public Product_info Product_info
+        {
+            get; set;
+        }
+        [XmlElement(ElementName = "partition_info")]
+        public Partition_info Partition_info
+        {
+            get; set;
+        }
+        [XmlElement(ElementName = "builds_flat")]
+        public Builds_flat Builds_flat
+        {
+            get; set;
+        }
+        [XmlElement(ElementName = "build_tools")]
+        public string Build_tools
+        {
+            get; set;
+        }
+        [XmlElement(ElementName = "external_tools")]
+        public External_tools External_tools
+        {
+            get; set;
+        }
+        [XmlElement(ElementName = "workflow")]
+        public Workflow Workflow
+        {
+            get; set;
+        }
+    }
+}
